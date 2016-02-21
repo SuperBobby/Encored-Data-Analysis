26eb1ac15c54b859257f1574a615d3cec6a401a7
SuperBobby <hanjy@snu.ac.kr>
2016-02-21 22:20:56 +0900

    RealSense data preprocessing step 정비

--- plot RealSense data.R
+++ plot RealSense data.R
@@ -1,4 +1,6 @@
 # install.packages("lubridate")
+# install.packages("bit64")
+
 library(lubridate)
 library(plyr)
 library(ggplot2)
@@ -7,17 +9,53 @@ library(reshape2)
 library(gridExtra)
 library(data.table)
 library(timeDate)
+require(bit64)
 
-make.RealSense.Table = function(data){
+table.time2string <- function(RS_raw_data){
+        RS_raw_data$joined = as.POSIXct(RS_raw_data$joined/1000, origin = "1970-01-01", tz="ROK")
+        RS_raw_data$leaved = as.POSIXct(RS_raw_data$leaved/1000, origin = "1970-01-01", tz="ROK")
+        
+        duration = difftime(RS_raw_data$leaved, RS_raw_data$joined, units = "secs")
+        
+        return(cbind(RS_raw_data, duration))
+}
+
+make.quarter.label = function(input){ 
+        # 15min data label maker 
+        h = floor(((input-1) * 15)/60)
+        m = (input-1)*15 - h*60
+        l = paste0(h,":",m)
+        return(l)
+}
+
+fill.empty.date <- function(input_dt){
+        input_dt$date = as.Date(input_dt$date)
+        
+        date_start = input_dt$date[1]
+        date_long  = as.numeric(as.Date(input_dt$date[nrow(input_dt)]) - as.Date(input_dt$date[1]))
+        
+        # print(paste(date_start, date_long))
+        full_date_index = data.frame(date = date_start + (0:date_long))
+        
+        result_df = merge(full_date_index, input_dt, all.x=T)
+        result_df[is.na(result_df)] = 0
+        
+        result_df = cbind(result_df, weekday = as.factor(isWeekday(result_df$date)))
+        print(head(result_df)); print(tail(result_df))
+        return(result_df)
+}
+
+
+build.RealSense.table = function(data){
         # convert data format
         joined = as.POSIXct(data$joined/1000, format="%Y-%m-%d", origin='1970-01-01', tz="ROK")
         leaved = as.POSIXct(data$leaved/1000, format="%Y-%m-%d", origin='1970-01-01', tz="ROK")
         duration = as.numeric(data$leaved - data$joined)
         
         basic_table = data.frame(joined, duration)
-
-#         basic_table = basic_table[basic_table$duration > threshold, ]
-#         basic_table = basic_table[basic_table$duration < threshold2, ]
+        
+        #         basic_table = basic_table[basic_table$duration > threshold, ]
+        #         basic_table = basic_table[basic_table$duration < threshold2, ]
         # print(nrow(basic_table))
         print(paste("range:", range(duration)))
         
@@ -46,54 +84,46 @@ make.RealSense.Table = function(data){
         
         return(return_table)
 }
-
-time2string <- function(input){
-        output = as.POSIXct(input/1000, origin = "1970-01-01", tz="ROK")
-}
-
 #####################
 # raw data loading
-# RS_adsl_raw = read.csv("realsense/adsl.csv")
-RS_marg_raw = read.csv("realsense/marg.csv")
-RS_hcc_raw = read.csv("realsense/hcc.csv")
-RS_ux_raw = read.csv("realsense/ux.csv")
+RS_adsl_raw = fread("realsense/adsl.csv")
+RS_marg_raw = fread("realsense/marg.csv")
+RS_hcc_raw = fread("realsense/hcc.csv")
+RS_ux_raw = fread("realsense/ux.csv")
 
-RS_marg_table = make.RealSense.Table(RS_marg_raw)
-RS_hcc_table  = make.RealSense.Table(RS_hcc_raw)
-RS_ux_table   = make.RealSense.Table(RS_ux_raw)
+RS_adsl = table.time2string(RS_adsl_raw)
+RS_marg = table.time2string(RS_marg_raw)
+RS_hcc = table.time2string(RS_hcc_raw)
+RS_ux = table.time2string(RS_ux_raw)
 
 ###################
 ## preprocessing 
 
-# nrow(RS_marg_table[RS_marg_table$duration > 1,])
-# nrow(RS_hcc_table[RS_hcc_table$duration > 1,])
-# nrow(RS_ux_table[RS_ux_table$duration > 1,])
-
-# View(RS_marg)
-# View(RS_hcc)
-# View(RS_ux)
-
-##############
-## plotting
-
-fill.empty.date <- function(input_dt){
-        input_dt$date = as.Date(input_dt$date)
+duration.filter <- function(dt, threshold_min, threshold_max){
+        filtered_min = nrow(dt[duration <= threshold_min])
+        filtered_max = nrow(dt[duration >= threshold_max])
         
-        date_start = input_dt$date[1]
-        date_long  = as.numeric(as.Date(input_dt$date[nrow(input_dt)]) - as.Date(input_dt$date[1]))
+        print(paste("filtered", filtered_min, "and", filtered_max, "rows (by min & max, respectively"))
         
-        # print(paste(date_start, date_long))
-        full_date_index = data.frame(date = date_start + (0:date_long))
-        
-        result_df = merge(full_date_index, input_dt, all.x=T)
-        result_df[is.na(result_df)] = 0
-        
-        result_df = cbind(result_df, weekday = as.factor(isWeekday(result_df$date)))
-        print(head(result_df)); print(tail(result_df))
-        return(result_df)
+        return(dt[duration > threshold_min & duration < threshold_max])
 }
 
-plot.RealSense.data <- function(lab, data, ylim, date_cut, type) {
+RS_marg = duration.filter(RS_marg, 1, 300)
+RS_hcc  = duration.filter(RS_hcc,  1, 300)
+RS_ux   = duration.filter(RS_ux,   1, 300)
+
+#######################
+## Build a 15min table 
+
+RS_marg_table = build.RealSense.table(RS_marg_raw)
+RS_hcc_table  = build.RealSense.table(RS_hcc_raw)
+RS_ux_table   = build.RealSense.table(RS_ux_raw)
+
+
+###################
+## plot functions
+
+lineplot.RealSense.data <- function(lab, data, ylim, date_cut, type) {
         
         dt = data.table(data)
         
@@ -156,15 +186,58 @@ plot.RealSense.data <- function(lab, data, ylim, date_cut, type) {
         return(plotting)
 }
 
+heatmap.RealSense.data <- function(lab, data, date_cut, threshold) {
+        
+        dt = data.table(data)
+        names(dt) <- c("quarter_index", "freq", "sum_of_duration")
+        
+        dt = dt[sum_of_duration > threshold]
+        dt = dt[quarter_index > date_cut]
+        
+        start_ts = as.numeric(dt$quarter_index[1])
+        end_ts   = as.numeric(dt$quarter_index[nrow(dt)])
+        
+        full_quarter_index = data.table(quarter_index = as.POSIXct(seq(from=start_ts, to=end_ts, by = 15*60), 
+                                                                   origin="1970-01-01", tz="ROK"))
+        setkey(full_quarter_index, quarter_index)
+        setkey(dt, quarter_index)
+        
+        # tables();
+        
+        dt = merge(full_quarter_index, dt, all.x=T)
+        dt = cbind(dt, date_index = as.Date(dt$quarter_index, tz="ROK"))
+        dt[, ':='(quarter = ((hour(quarter_index) * 60 + minute(quarter_index)) / 15) + 1), by=.(date_index)]
+        
+        # View(dt)
+        
+        plotting <- ggplot(dt, aes(x=date_index, y=quarter, fill=sum_of_duration)) +
+                scale_x_date(date_labels = "%y-%b-%d", breaks = date_breaks("days")) +
+                scale_y_continuous(breaks=1:96, labels=make.quarter.label(1:96)) + 
+                theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+                geom_raster() +
+                ggtitle(paste(lab, "RealSense heatmap ( threshold:", threshold, ")" ))
+        
+        print(plotting)
+}
+
+
+##################
+## plotting 
 
 # max_threshold = 500
 date_cut = "2015-10-30"
 # date_cut = "2015-12-31"
 type = "duration_sum" # freq, duration_mean, duration_sum
 
-marg <- plot.RealSense.data("MARG", RS_marg_table, ylim = 600, "2015-10-07", "duration_sum")
-hcc <- plot.RealSense.data("HCC", RS_hcc_table, ylim = 600, "2015-10-28", "duration_sum")
-ux <- plot.RealSense.data("UX", RS_ux_table, ylim = 600, "2015-09-30", "duration_sum")
+marg <- lineplot.RealSense.data("MARG", RS_marg_table, ylim = 600, "2015-10-07", "duration_sum")
+hcc <- lineplot.RealSense.data("HCC", RS_hcc_table, ylim = 600, "2015-10-28", "duration_sum")
+ux <- lineplot.RealSense.data("UX", RS_ux_table, ylim = 600, "2015-09-30", "duration_sum")
 
 grid.arrange(marg, hcc, ux)
+
+
+heatmap.RealSense.data("MARG", RS_marg_table, date_cut = "2015-10-07", threshold = 1)
+heatmap.RealSense.data("HCC", RS_hcc_table, date_cut = "2015-10-07", threshold = 1)
+heatmap.RealSense.data("UX", RS_ux_table, date_cut = "2015-10-07", threshold = 1)
+
 
