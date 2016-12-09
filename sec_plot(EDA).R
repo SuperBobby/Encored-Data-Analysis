cf5ccaad1fb5eba88a4393b233b055caaa90627a
Han Jeong Yun <hanjy@snu.ac.kr>
2016-12-10 08:41:36 +0900

    bug fix

--- sec_plot(EDA).R
+++ sec_plot(EDA).R
@@ -2,7 +2,6 @@ library(data.table)
 library(ggplot2)
 library(zoo)
 
-N_of_feeder = length(milli_dt) - 1
 
 PRE_POST_WIDTH = 30 # secs
 HIGHLIGHT_WIDTH = 5 # secs 
@@ -11,9 +10,6 @@ PRE_POST_HEIGHT_THRE = 30 # Watts
 
 ON_OFF_GAP_THRE   = 80 # Watts
 PRE_POST_GAP_THRE = 50 # Watts
-
-STARTING_INDEX = PRE_POST_WIDTH + HIGHLIGHT_WIDTH + 1
-FINISHING_INDEX = nrow(one_com_feeder_dt) - PRE_POST_WIDTH - HIGHLIGHT_WIDTH
 
 get.com.status = function(pre, highlight, post){
   
@@ -49,7 +45,7 @@ get.com.status = function(pre, highlight, post){
            pre_med=pre_med, 
            post_height=post_height, 
            post_med=post_med, 
-           return_status=return_status))
+           return_status=return_status)) # 7 returns
 }
 
 
@@ -57,23 +53,33 @@ get.com.status = function(pre, highlight, post){
 ## LoooooooooooooooooooooooP~!
 ##
 
+LOOP_START = numeric(0)
+LOOP_END = numeric(0)
 
 lab_labels = c('marg', 'hcc', 'ux')
 
 for(lab in lab_labels){
  
   ## initialize status_dt
-  status_dt = data.frame(matrix(data=NA, nrow=1, ncol=8))
-  names(status_dt) <-c("lab", "on_off_gap", "pre_post_gap", "pre_height", "pre_med", "post_height", "post_med", "return_status")
+  status_dt = data.frame(matrix(data=NA, nrow=1, ncol=10))
+  names(status_dt) <-c("lab", "target_feeder", "dts", "on_off_gap", "pre_post_gap", "pre_height", "pre_med", "post_height", "post_med", "return_status")
    
   ## load input file 
   input_file_name = paste0("../data/sec_tidy/", lab, "_2015-09-01(com).csv")  
-  com_dt = fread(input_file_name)
+  com_dt = fread(input_file_name, nrows = -1)
+  
+  N_of_feeder = length(com_dt) - 1
   
   ## loop for each computer feeder
   for(target_feeder in 1:N_of_feeder){
+    
+    LOOP_START = c(LOOP_START, Sys.time())
+    
     one_com_feeder_dt = com_dt[, c(1,target_feeder+1),with=F]
     one_com_feeder_dt = cbind(one_com_feeder_dt, status = 0)
+    
+    STARTING_INDEX = PRE_POST_WIDTH + HIGHLIGHT_WIDTH + 1
+    FINISHING_INDEX = nrow(one_com_feeder_dt) - PRE_POST_WIDTH - HIGHLIGHT_WIDTH
     
     for(index in STARTING_INDEX:(FINISHING_INDEX)){
       com_usage = unlist(one_com_feeder_dt[,2, with=F]) / 1000
@@ -84,9 +90,10 @@ for(lab in lab_labels){
       post      = com_usage[(index+HIGHLIGHT_WIDTH+1):(index+PRE_POST_WIDTH+HIGHLIGHT_WIDTH)]
       
       status_vector = get.com.status(pre, highlight, post)
-      status_dt = rbind(status_dt, c(lab, status_vector))
+      status_dt = rbind(status_dt, c(lab, target_feeder, one_com_feeder_dt[index]$dts,
+                                     status_vector)) # 3 + 7 columns 
       
-      one_com_feeder_dt[index]$status = status_vector[7]
+      one_com_feeder_dt[index]$status = status_vector[length(status_vector)]
       
       # show the current status change at console  
       if(index %% 60 == 0| one_com_feeder_dt[index]$status != 0) {
@@ -102,7 +109,7 @@ for(lab in lab_labels){
     unit = 200
     loop_max = nrow(dt_for_plot) / unit
     
-    com_feeder_name = paste0(lab, '_com1')
+    com_feeder_name = paste0(lab, '_com', target_feeder)
     
     max_value = range(com_usage)[2] * 0.8 
     min_value = range(com_usage)[1]
@@ -121,12 +128,15 @@ for(lab in lab_labels){
       # print(p)
       ggsave(filename = paste0("../plots/milli/", lab, target_feeder,'-', i, ".png"), plot = p, width = 50, height = 10, units='cm')
     }
+
+    LOOP_END = c(LOOP_END, Sys.time())    
   }
   
   write.csv(status_dt, paste0('../data/status_dt(', lab,').csv'), row.names = F)
   
 }
 
+print(LOOP_END - LOOP_START)
 
 
 
